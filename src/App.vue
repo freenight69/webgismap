78a995e3297860c6c983ecb83303f3630d45fcbd
freenight69 <gongchen9369@163.com>
2022-12-02 09:48:34 +0800

    初始化项目结构

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <span>test</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {}
+}
+</script>
+
+<style>
+html, body, #app {
+  position: relative;
+  widows: 100%;
+  height: 100%;
+  margin: 0;
+}
+</style>
